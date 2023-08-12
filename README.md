87da2ac535fee523f880f09fd2311f373a3d25f4
Amodu <amodudavid777@gmail.com>
2023-08-12 08:41:38 +0100

    added a readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Movie-Rating
+
+> Red underlines? Install the required packages first: 
+
+
+* On Windows type:
+python -m pip install -r requirements.txt
+
+* On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+
+> To create Database
+ * uncomment line 19 and 20 then run
--- main.py
+++ main.py
@@ -5,18 +5,7 @@ from form import UpdateForm, AddMovie
 import requests
 import os
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 URL = "https://api.themoviedb.org/3/search/movie"
 API_KEY = os.environ.get('api_key')
 
